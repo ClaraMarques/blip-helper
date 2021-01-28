304fd0b9a9b706821871b8150dae4a0259d6d73d
Clara <claradomingues29@gmail.com>
2021-01-28 09:28:16 -0300

    removing sensitive data

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const helper = require("./blip-helper-attendants.js");
 const helperUser = require("./blip-helper-user");
 
-helper.migrateAttendanceRules(
-  "Key c2ltaG1nYXRlbmRpbWVudG86VzdHTWhBQ2hqUkVVSjNDWVh1R04=",
-  "Key dGVzdGUyNDE5OlV4SFdzOEZlVll1MXpCTFJEeXRC"
-);
+
